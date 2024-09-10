6e1707e6385e8780985f8e16976af94b3584364f
nt_56 <nagutirth2121@gmail.com>
2024-09-10 09:18:30 +0530

    Build Main Container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
-  return <div>MainContainer</div>;
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  if (!movies) return;
+
+  const mainMovie = movies[0];
+
+  const { original_title, overview, id } = mainMovie;
+
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
 };
 
 export default MainContainer;
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,30 @@
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  //fetch trailer video
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    //filtering the trailer only
+    const videoData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = videoData.length ? videoData[0] : json.results[0]; //some movies has more than one trailer so i take first and if no trailer then show first clip by default
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+};
+
+export default useMovieTrailer;
